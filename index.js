a3d07ac87aea45daeb22dbb2edba4f315b8adcb4
mark langan <markelangan@gmail.com>
2023-05-03 11:27:47 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record){
+   const found = record.find((object)=>object.result ==="W");
+   console.log("this is the value of found:", found)
+   if (found===undefined) {
+    return found
+   } else return found.year
+}
+    
